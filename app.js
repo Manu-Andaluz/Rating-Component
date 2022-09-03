c30d2c51b3ca5f77ac2e14bc2b2ab78630ce8b77
manu-andaluz <manupeandaluz@gmail.com>
2022-09-03 16:52:20 -0300

    creating the element with javascript

--- app.js
+++ app.js
@@ -1,23 +1,54 @@
-// Rating numbers hover events
-let ratingElement = document.getElementsByClassName('ratingElement')
-console.dir(ratingElement)
-function changeBackgrund() {
-    for (let i = 0; i <= ratingElement.length - 1; i++) {
-        ratingElement[i].addEventListener('mouseover', (e) => {
-            ratingElement[i].classList.add('active')
-        })
+const body = document.body
 
-        ratingElement[i].addEventListener('mouseleave', (e) => {
-            ratingElement[i].classList.remove('active')
-        })
-    }
+const container = document.createElement('div')
+container.classList.add('container')
+body.append(container)
+
+const gridLayout = document.createElement('div')
+gridLayout.classList.add('grid_layout')
+container.append(gridLayout)
+
+const fristIcon = document.createElement('img')
+fristIcon.src = 'images/icon-star.svg'
+fristIcon.classList.add('icon_star')
+
+const title = document.createElement('h2')
+title.innerText = 'How did we do?'
+
+const aboutText = document.createElement('p')
+aboutText.innerText = 'Please let us know how we did with your support request. All feedback is appreciated to help us improve ouroffering!'
+
+const containerRating = document.createElement('div')
+containerRating.classList.add('container_rating')
+
+for (let i = 1; i <= 5; i++) {
+    const ratingElement = document.createElement('a')
+    ratingElement.innerText = i
+    containerRating.append(ratingElement)
 }
 
-changeBackgrund()
+gridLayout.append(fristIcon, title, aboutText, containerRating)
 
-// Button submit event
+// Rating numbers hover events
+// let ratingElement = document.getElementsByClassName('ratingElement')
+// console.dir(ratingElement)
+// function changeBackgrund() {
+//     for (let i = 0; i <= ratingElement.length - 1; i++) {
+//         ratingElement[i].addEventListener('mouseover', (e) => {
+//             ratingElement[i].classList.add('active')
+//         })
 
-const button = document.getElementById('Btn')
-button.addEventListener('click', () => {
+//         ratingElement[i].addEventListener('mouseleave', (e) => {
+//             ratingElement[i].classList.remove('active')
+//         })
+//     }
+// }
 
-})
+// changeBackgrund()
+
+// // Button submit event
+
+// const button = document.getElementById('Btn')
+// button.addEventListener('click', () => {
+
+// })
